3b58ee5781bcbda278a758235a389a0ef9e9ac9a
Sumit Roy <sroy8091@gmail.com>
2016-12-16 23:11:39 +0530

    buggy

--- urlshortner/settings.py
+++ urlshortner/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,7 +75,8 @@ WSGI_APPLICATION = 'urlshortner.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
